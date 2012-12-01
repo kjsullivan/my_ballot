6b06f529560e17e51c1cf30a1fa1f5a8234915aa
Karen Sullivan <ksullivan@redsquareagency.com>
2012-12-01 13:39:58 -0600

    Added ballot info to user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :logged_in?
+  
+  def logged_in?
+    session[:id]
+  end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def current_user
+     @user ||= User.find_by_id(session[:id]) if logged_in?
+   end
+  
 end
